bc35ca2c254fb70d9656e4b12a6bba28bfe6f1fd
ame <shirohako@qq.com>
2019-12-05 12:39:25 +0800

    update libsodium.sh

--- libsodium.sh
+++ libsodium.sh
@@ -9,13 +9,38 @@ export PATH
 #	Author: ame
 #=================================================
 
+
+if [[ -f /etc/redhat-release ]]; then
+        release="centos"
+elif cat /etc/issue | grep -q -E -i "debian"; then
+        release="debian"
+elif cat /etc/issue | grep -q -E -i "ubuntu"; then
+        release="ubuntu"
+elif cat /etc/issue | grep -q -E -i "centos|red hat|redhat"; then
+        release="centos"
+elif cat /proc/version | grep -q -E -i "debian"; then
+        release="debian"
+elif cat /proc/version | grep -q -E -i "ubuntu"; then
+        release="ubuntu"
+elif cat /proc/version | grep -q -E -i "centos|red hat|redhat"; then
+        release="centos"
+fi
+
+if [[ $release == centos ]]; then
+	yum update
+    yum -y groupinstall "Development Tools"
+else
+	apt-get update
+    apt-get install -y build-essential
+fi
+
 GreenPrefix="\033[42m" && RedPrefix="\033[41m" && BluePrefix="\033[44m" && Suffix="\033[0m"
 
 LATEST_RELEASE=$(wget -qO- "https://github.com/jedisct1/libsodium/tags"|grep "/jedisct1/libsodium/releases/tag/"|head -1|sed -r 's/.*tag\/(.+)\">.*/\1/') 
 
-echo -e "${BluePrefix}Downloading libsodium latest release:${LATEST_RELEASE}${Suffix}"
+echo -e "${BluePrefix}Downloading libsodium ${LATEST_RELEASE} ${Suffix}"
 
-wget --no-check-certificate -N "https://github.com/jedisct1/libsodium/archive/${LATEST_RELEASE}.tar.gz"
+wget --no-check-certificate "https://github.com/jedisct1/libsodium/archive/${LATEST_RELEASE}.tar.gz"
 
 tar -xzf ${LATEST_RELEASE}.tar.gz && cd libsodium-${LATEST_RELEASE}
 ./configure --disable-maintainer-mode && make -j2 && make install
