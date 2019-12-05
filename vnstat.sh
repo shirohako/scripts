0914e65b4c565843149d964e0430aeaec48be930
ame <shirohako@qq.com>
2019-12-05 11:47:25 +0800

    update vnstat.sh

--- vnstat.sh
+++ vnstat.sh
@@ -37,18 +37,13 @@ else
     apt-get install wget make gcc libc6-dev libgd-dev libsqlite3-0 libsqlite3-dev ntpdate -y
 fi
 
-<<<<<<< HEAD
 wget -N --no-check-certificate https://humdi.net/vnstat/vnstat-latest.tar.gz
-=======
-wget -N https://humdi.net/vnstat/vnstat-latest.tar.gz
->>>>>>> 69344326470f018d3430f25f299def3d22ea0390
 tar zxvf vnstat-latest.tar.gz
 rm vnstat-latest.tar.gz -f
 cd vnstat-2*
 ./configure --prefix=/usr --sysconfdir=/etc && make && make install
 
 if [[ ! -f /usr/bin/vnstat ]]; then
-<<<<<<< HEAD
    echo -e "${RedPrefix}Installation has failed!${Suffix}"
    exit 1
 fi
@@ -57,16 +52,6 @@ cp /usr/share/zoneinfo/Asia/Shanghai  /etc/localtime
 ntpdate asia.pool.ntp.org
 
 wget -N --no-check-certificate https://raw.githubusercontent.com/vergoh/vnstat/master/examples/systemd/simple/vnstat.service
-=======
-   echo "安装失败!"
-   exit 1
-fi
-
-cp /usr/share/zoneinfo/Asia/Shanghai  /etc/localtime
-ntpdate asia.pool.ntp.org
-
-wget https://raw.githubusercontent.com/vergoh/vnstat/master/examples/systemd/simple/vnstat.service
->>>>>>> 69344326470f018d3430f25f299def3d22ea0390
 chmod 754 vnstat.service && mv vnstat.service /etc/systemd/system -f
 systemctl enable vnstat && systemctl start vnstat
 systemctl daemon-reload
